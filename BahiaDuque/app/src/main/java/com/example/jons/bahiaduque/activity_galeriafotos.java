77aa8daaabfd47f1d09d3847523eef20b44d78cd
jolome777 <77jonas77@gmail.com>
2015-11-19 16:09:11 +0000

    Funcionalidad del scroll view

--- BahiaDuque/app/src/main/java/com/example/jons/bahiaduque/activity_galeriafotos.java
+++ BahiaDuque/app/src/main/java/com/example/jons/bahiaduque/activity_galeriafotos.java
@@ -4,15 +4,63 @@ import android.annotation.TargetApi;
 import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class activity_galeriafotos extends ActionBarActivity {
+import java.util.ArrayList;
+
+public class activity_galeriafotos extends ActionBarActivity implements View.OnClickListener {
+
+    ImageButton tmp1;
+    ImageButton tmp2;
+    ImageButton tmp3;
+    ImageButton tmp4;
+    ImageButton tmp5;
+    ImageButton tmp6;
+    ImageButton tmp7;
+    ImageButton tmp8;
+    ImageButton tmp9;
+    ImageButton tmp10;
+    ImageButton tmp11;
+    ImageButton tmp12;
+    ImageView imagen;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_galeriafotos);
+
+        tmp1 = (ImageButton) findViewById(R.id.imageButton1);
+        tmp1.setOnClickListener(this);
+        tmp2 = (ImageButton) findViewById(R.id.imageButton2);
+        tmp2.setOnClickListener(this);
+        tmp3 = (ImageButton) findViewById(R.id.imageButton3);
+        tmp3.setOnClickListener(this);
+        tmp4 = (ImageButton) findViewById(R.id.imageButton4);
+        tmp4.setOnClickListener(this);
+        tmp5 = (ImageButton) findViewById(R.id.imageButton5);
+        tmp5.setOnClickListener(this);
+        tmp6 = (ImageButton) findViewById(R.id.imageButton6);
+        tmp6.setOnClickListener(this);
+        tmp7 = (ImageButton) findViewById(R.id.imageButton7);
+        tmp7.setOnClickListener(this);
+        tmp8 = (ImageButton) findViewById(R.id.imageButton8);
+        tmp8.setOnClickListener(this);
+        tmp9 = (ImageButton) findViewById(R.id.imageButton9);
+        tmp9.setOnClickListener(this);
+        tmp10 = (ImageButton) findViewById(R.id.imageButton10);
+        tmp10.setOnClickListener(this);
+        tmp11 = (ImageButton) findViewById(R.id.imageButton11);
+        tmp11.setOnClickListener(this);
+        tmp12 = (ImageButton) findViewById(R.id.imageButton12);
+        tmp12.setOnClickListener(this);
+        imagen = (ImageView) findViewById(R.id.imagen1);
+
     }
 
     @Override
@@ -43,5 +91,48 @@ public class activity_galeriafotos extends ActionBarActivity {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageButton1:
+                imagen.setImageResource(R.drawable.foto1);
+            break;
+            case R.id.imageButton2:
+                Log.i("MyActivity", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                imagen.setImageResource(R.drawable.foto2);
+            break;
+            case R.id.imageButton3:
+                imagen.setImageResource(R.drawable.foto3);
+            break;
+            case R.id.imageButton4:
+                imagen.setImageResource(R.drawable.foto1);
+            break;
+            case R.id.imageButton5:
+                imagen.setImageResource(R.drawable.foto2);
+            break;
+            case R.id.imageButton6:
+                imagen.setImageResource(R.drawable.foto3);
+            break;
+            case R.id.imageButton7:
+                imagen.setImageResource(R.drawable.foto1);
+            break;
+            case R.id.imageButton8:
+                imagen.setImageResource(R.drawable.foto2);
+            break;
+            case R.id.imageButton9:
+                imagen.setImageResource(R.drawable.foto3);
+            break;
+            case R.id.imageButton10:
+                imagen.setImageResource(R.drawable.foto1);
+            break;
+            case R.id.imageButton11:
+                imagen.setImageResource(R.drawable.foto2);
+            break;
+            case R.id.imageButton12:
+                imagen.setImageResource(R.drawable.foto3);
+            break;
+        }
     }
 }
