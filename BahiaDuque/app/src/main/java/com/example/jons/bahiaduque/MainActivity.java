d9848f3a29e38e5652154b3f9e1f66fe64542efa
jolome777 <77jonas77@gmail.com>
2015-11-23 13:56:50 +0000

    Añadido el Splash Screen y el selector de idioma
    Los dos son la misma activity

--- BahiaDuque/app/src/main/java/com/example/jons/bahiaduque/MainActivity.java
+++ BahiaDuque/app/src/main/java/com/example/jons/bahiaduque/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -25,6 +27,7 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+
 
     }
 
---
+++ BahiaDuque/app/src/main/java/com/example/jons/bahiaduque/SplashScreen.java
@@ -0,0 +1,76 @@
+package com.example.jons.bahiaduque;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class SplashScreen extends ActionBarActivity implements View.OnClickListener {
+
+
+    private ImageButton spanish;
+    private ImageButton france;
+    private ImageButton germany;
+    private ImageButton italy;
+    private int request;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        spanish = (ImageButton) findViewById(R.id.imageButton14);
+        spanish.setOnClickListener(this);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Selecciona un idioma",
+                Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 120);
+        toast.show();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // In KITKAT (4.4) and next releases, hide the virtual buttons
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                hideVirtualButtons();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideVirtualButtons();
+    }
+
+    @TargetApi(19)
+    public void hideVirtualButtons() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageButton14:
+                Intent intent1 = new Intent(SplashScreen.this, MainActivity.class);
+                startActivityForResult(intent1, request);
+                break;
+        }
+    }
+}
