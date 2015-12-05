616b7f646200fc0d0a305b1628e8f2c6be392cd0
jolome777 <77jonas77@gmail.com>
2015-12-05 12:43:32 +0000

    Modificado aspecto del activity mapas

--- BahiaDuque/app/src/main/java/com/example/jons/bahiaduque/ActivityMapa.java
+++ BahiaDuque/app/src/main/java/com/example/jons/bahiaduque/ActivityMapa.java
@@ -1,7 +1,11 @@
 package com.example.jons.bahiaduque;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class ActivityMapa extends ActionBarActivity {
 
@@ -9,5 +13,36 @@ public class ActivityMapa extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_mapa);
+        hideVirtualButtons();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // In KITKAT (4.4) and next releases, hide the virtual buttons
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                hideVirtualButtons();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideVirtualButtons();
+    }
+
+    @TargetApi(19)
+    public void hideVirtualButtons() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
